2c73b6b7432c0d0e336e7ee10622d6bc5c891d91
Branislav Milenkovic <branislav.milenkovic@gmail.com>
2017-09-30 17:49:51 -0500

    call api gateway endpoints

--- app/consultant-detail/consultant-detail.component.js
+++ app/consultant-detail/consultant-detail.component.js
@@ -5,10 +5,13 @@ angular.
   module('consultantDetail').
   component('consultantDetail', {
     templateUrl: 'consultant-detail/consultant-detail.template.html',
-    controller: ['$routeParams', 'Consultant',
-      function ConsultantDetailController($routeParams, Consultant) {
+    controller: ['$routeParams', 'Consultant', '$http',
+      function ConsultantDetailController($routeParams, Consultant, $http) {
         var self = this;
-        self.consultant = Consultant.get({consultantId: $routeParams.consultantId});
+        // self.consultant = Consultant.get({consultantId: $routeParams.consultantId});
+          $http.get('https://jq8u7kee62.execute-api.us-east-2.amazonaws.com/DEV/consultants/' + $routeParams.consultantId).then(function(response) {
+              self.consultant = response.data;
+          });
       }
     ]
  });
--- app/consultant-list/consultant-list.component.js
+++ app/consultant-list/consultant-list.component.js
@@ -2,13 +2,13 @@
 
 // Register `consultantList` component, along with its associated controller and template
 angular.
-  module('consultantList').
-  component('consultantList', {
+module('consultantList').
+component('consultantList', {
     templateUrl: 'consultant-list/consultant-list.template.html',
-    controller: ['Consultant',
-      function ConsultantListController(Consultant) {
-        this.consultants = Consultant.query();
-        this.orderProp = 'order';
-      }
-    ]
-  });
+    controller: function ConsultantListController($http) {
+        var self = this;
+         $http.get('https://jq8u7kee62.execute-api.us-east-2.amazonaws.com/DEV/consultants').then(function(response) {
+            self.consultants = response.data;
+        });
+        self.orderProp = 'order';
+    }});
